1f93a9399dc392a60e7d72ceecd8d7a16d0fe8e8
SEBA-Sonia-Bouchra <ns_seba@esi.dz>
2025-05-11 20:57:27 +0100

    added secure approval of the admin

--- frontend/src/pages/AccountsPage.jsx
+++ frontend/src/pages/AccountsPage.jsx
@@ -42,10 +42,12 @@ const AccountsPage = () => {
     }, []);
 
     const updateStatus = (id, action) => {
+      const token = sessionStorage.getItem("token"); 
       fetch(`http://localhost:5000/api/auth/approve`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`, 
         },
         body: JSON.stringify({ userId: id, action }),
       })
